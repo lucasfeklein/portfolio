e74cf4108430f34d7fdb3d51f5311b9086ef7d21
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-01 17:02:39 +0700

    hero section button

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   const { progress } = useProgress();
   return (
     <Html>
-      <span className="canvas-load"></span>
+      <span className="canvas-loader"></span>
       <p
         style={{
           fontSize: 14,
