ed1d016ead4dd888f987b45bc088f98407000899
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-02 11:03:08 +0700

    start about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+import { services } from "../constants";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I still have no idea what i do, i'm a beginner ande have 0 skills so you
+        better hire me otherwise i'm going to die from starvation. I learn fast
+        and have the desire to learn every thing thats going to help me
+      </motion.p>
+
+      <div className="mt-20 flex-wrap flex gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
