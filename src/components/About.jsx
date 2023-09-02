53239cb27cff9f8ff7389d59e2c20e88bb9207b5
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-02 11:25:56 +0700

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ const ServiceCard = ({ index, title, icon }) => {
           options={{
             max: 45,
             scale: 1,
-            speed: 1000,
+            speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-around items-center flex-col"
         >
