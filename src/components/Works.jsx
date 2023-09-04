d74d7966d6416c3747f118dc4dc4cd575435dc64
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-04 11:41:58 +0700

    img and github icon in Project Cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+import { github } from "../assets";
 import { projects } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
@@ -14,7 +16,31 @@ const ProjectCard = ({
 }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      Test
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+        </div>
+
+        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img
+              src={github}
+              alt="github"
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
+        </div>
+      </Tilt>
     </motion.div>
   );
 };
