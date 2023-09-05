76b808a49aeed2480e57cab9e7b8a3037b789489
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-05 09:21:18 +0700

    contact section start

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useRef, useState } from "react";
+import { SectionWrapper } from "../hoc";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-export default Contact
+  const handleChange = (e) => {};
+
+  const handleSubmite = (e) => {};
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"></div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
