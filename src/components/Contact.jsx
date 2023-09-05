d800586c7fcb2114378386034f8dda362d00d919
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-05 09:35:27 +0700

    subtitle effect contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,8 @@
+import { motion } from "framer-motion";
 import { useRef, useState } from "react";
 import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
   const formRef = useRef();
@@ -14,7 +17,15 @@ const Contact = () => {
 
   const handleSubmite = (e) => {};
   return (
-    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden"></div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
   );
 };
 
