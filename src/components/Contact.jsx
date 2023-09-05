db52bcba67c7300a0731413d1cf10798b90418a2
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-05 10:40:38 +0700

    contact form init

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,17 @@ const Contact = () => {
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmite}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Name</span>
+            <input type="text" name="name" value={form.name} />
+          </label>
+        </form>
       </motion.div>
     </div>
   );
