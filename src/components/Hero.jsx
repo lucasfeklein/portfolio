906d0294fb31ba4410d35ec6a697ece5ef7540b3
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-08 15:09:54 +0700

    responsive style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 h-80 violet-gradient" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
